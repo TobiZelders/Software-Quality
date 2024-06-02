a8d7a1d46df74402e54fd77545651304d5d50afe
Tobiaszelders <58260564+Tobiaszelders@users.noreply.github.com>
2024-06-02 20:03:18 +0200

    Updated hash_password (to hash a password) And added verify_password (to check a provided password with a stored password from database)
    removed files from previous group member

--- Program/secuirity.py
+++ Program/secuirity.py
@@ -1,6 +1,7 @@
 import random
 import datetime
 import hashlib
+import os
 import re
 from db import create_connection
 
@@ -75,7 +76,47 @@ for password in incorrect_passwords:
         print(f"'{password}' is an invalid password")
 
 def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+    """
+    This function will return a hash of the provided password
+
+    When storing the hashed password we should also store salt to be able to verify later
+    advised format = salt:hashed_password
+
+    :param password: provided password by user (recommended to first use password_regex)
+    :return: hashed password
+    """
+
+    byte_password = password.encode()
+    salt = os.urandom(16)
+    iterations = 500_000
+    hash_name = 'sha256'
+    derived_key_length = None
+
+    return salt, hashlib.pbkdf2_hmac(hash_name, byte_password, salt, iterations, derived_key_length)
+    #return hashlib.sha256(password.encode()).hexdigest()
+
+def verify_password(stored_password, input_password):
+    """
+    Verifies password provided by user with stored password in database
+
+    :param stored_password: hashed password stored in database
+    :param input_password: un-hashed password provided by user
+    :return: Boolean based on if the provided hashed password matches hashed password saved in database
+    """
+
+    salt = stored_password.split(':')[0]
+    hashed_password = stored_password.split(':')[1]
+    salt = bytes.fromhex(salt)
+    hashed_password = bytes.fromhex(hashed_password)
+
+    input_password = input_password.encode()
+    hash_name = 'sha256'
+    iterations = 500_000
+    derived_key_length = None
+
+    new_hashed_password = hashlib.pbkdf2_hmac(hash_name, hashed_password, salt, iterations, derived_key_length)
+
+    return new_hashed_password == input_password
 
 def add_user(username, password, role, first_name, last_name):
     conn = create_connection()
